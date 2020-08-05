ffac7115e6979e435d4a3534582457ee3a93035e
Anissa Shanks <anissa.shanks@gmail.com>
2020-08-04 22:58:01 -0500

    created days and weeks columns in javascript

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const caldendar = document.querySelector("#appointmentCalendar");
+const calendar = document.querySelector("#appointmentCalendar");
 
 for(let day = 1; day < 31; day = day +=1);
-console.log(day)
+// console.log("day")
 
-
+calendar.insertAdjacentHTML("beforeend", `<div class="day">${day}</div>`);
 
 
 // GIVEN I am using a daily planner to create a schedule
 
 // WHEN I open the planner
 	
-$( ".lead" ).show();
+
 
 
 // THEN the current day is displayed at the top of the calendar
 
-$.toDate();
 // Does this link to display class in html?
 
 // WHEN I scroll down
 
-$(selector).scroll(function)
+
 
 // THEN I am presented with timeblocks for standard business hours
 
@@ -51,9 +50,8 @@ $(selector).scroll(function)
 
 
 // WHEN I refresh the page
-$('#currentDay').click(function() {
-    location.reload();
-});
+
+
 
 
 // THEN the saved events persist
