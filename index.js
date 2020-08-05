ee5c266ceba07f2904638eb1e455317bd23b23b6
Anissa Shanks <anissa.shanks@gmail.com>
2020-08-04 21:26:07 -0500

    saved javascript html and read me files into new repository

---
+++ index.js
@@ -0,0 +1,51 @@
+// GIVEN I am using a daily planner to create a schedule
+
+// WHEN I open the planner
+	
+$( ".lead" ).show();
+
+
+// THEN the current day is displayed at the top of the calendar
+
+$.toDate();
+// Does this link to display class in html?
+
+// WHEN I scroll down
+
+$(selector).scroll(function)
+
+// THEN I am presented with timeblocks for standard business hours
+
+
+
+// WHEN I view the timeblocks for that day
+
+
+
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+
+
+// WHEN I click into a timeblock
+
+
+
+// THEN I can enter an event
+
+
+
+// WHEN I click the save button for that timeblock
+
+
+
+// THEN the text for that event is saved in local storage
+
+
+
+// WHEN I refresh the page
+$('#currentDay').click(function() {
+    location.reload();
+});
+
+
+// THEN the saved events persist
